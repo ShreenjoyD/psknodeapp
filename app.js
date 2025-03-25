2c47259e4099333265cbfeca1b90f0e6ee187e92
Shreenjoy <146005060+ShreenjoyD@users.noreply.github.com>
2025-03-25 16:17:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,66 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT=8000;
+const PORT= process.env.MY_PORT || 8000;
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs=require('fs');
-//const { MongoClient, ServerApiVersion } = require('mongodb');
-//const uri = "mongodb+srv://<username>:<Password>@cluster0.v53lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-//const uri = "mongodb+srv://salestimefortheweb:rDVL7CUcS8eMh9Ca@cluster0.v53lv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 app.use(express.json());
 
 app.use(cors());
 app.use(bodyParser.json());
-
-/*app.post('/brand', async(req, res) => {
-  const {brandname} = req.body;
-  const client = new MongoClient(uri, {
-  serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  await client.connect();
-  const c=await client.db("allbrands").collection('models').findOne({brand:brandname});
-  res.send(c.models);
-  await client.close();
-});
-
-app.post(`/brand/model/year`, async(req, res) => {
-  const bname = req.body.brandname;
-  const modelName = req.body.mname;
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  await client.connect();
-  const c=await client.db("allbrands").collection('years').findOne({brand:bname, model:modelName});
-  res.send(c.allyrs);
-  await client.close();
-});
-
-app.post(`/brand/model/year/power`, async(req, res) => {
-  const bname = req.body.brandname;
-  const modelName = req.body.selectedMod;
-  const year = req.body.yvalue;
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  await client.connect();
-  const c=await client.db("allbrands").collection('powers').findOne({brand:bname, model:modelName, year:year});
-  res.send(c.allpws);
-  await client.close();
-});*/
 
 app.post('/subscribd', (req, res) => {
     const { news } = req.body;
@@ -80,26 +28,15 @@ app.post('/subscribd', (req, res) => {
         html: '<h1>Thank You for subscribing to our Newsletter</h1>'
       };
 
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+      transporter.sendMail(mailOptions);
 });
 
 app.post('/contacts', (req, res)=>{
     const inputs = req.body;
     const i=JSON.stringify(inputs);
     let ob=JSON.parse(i);
-    fs.appendFile('queries.txt',`Name: ${ob.fname} ${ob.lname}\nEmail: ${ob.mail}\nPhone: ${ob.pnumber}\nMessage: ${ob.comment}\n\n\n`,function (err) {
-    if (err) throw err;
-      console.log('Saved!');
-    });
+    fs.appendFile('queries.txt',`Name: ${ob.fname} ${ob.lname}\nEmail: ${ob.mail}\nPhone: ${ob.pnumber}\nMessage: ${ob.comment}\n\n\n`);
     res.status(200);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost: ${PORT}`);
-});
+app.listen(PORT);
